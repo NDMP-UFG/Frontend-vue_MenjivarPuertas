72f8ec9444f34f0041417c472950fced10002462
NDMP-UFG <ad.vladdracul@ufg.edu.sv>
2024-10-29 22:42:37 -0600

    Add files via upload

---
+++ proyecto-vue-ndmp/vue.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require('@vue/cli-service')
+/*module.exports = defineConfig({
+  transpileDependencies: true
+})*/
+module.exports={
+  pwa:{
+  name:"Proyecto de Nestor David Menjivar Puertas",
+  themeColor: "#FFd100",
+  msTileColor: '#000000',
+  appleMobileWebAppCapable: 'yes',
+  appleMobileWebAppStatusBarStyle: 'black'
+  }
+}
